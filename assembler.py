8dfdf180596023e35a8150ddd5e7894c71a39b4f
smilekids <46591071+smilekids@users.noreply.github.com>
2020-11-17 09:25:57 +0700

    Add files via upload

---
+++ assembler.py
@@ -0,0 +1,21 @@
+import CodeType as c
+
+def Assembler(lineSplit,mem,PC,label_addr):                                  
+        if lineSplit[1] == 'add' or lineSplit[1] == 'nand'  :                          #เช็คตาม Type 
+            mem[PC] = int(c.Rtype(lineSplit),2)                                 #go to Type_code.py ได้เลขฐานสองมาทำเป็นเลขฐานสิบเก็บใน mem
+        elif  lineSplit[1] == 'lw' or lineSplit[1] == 'sw' or lineSplit[1] == 'beq' :   
+            mem[PC] = int(c.Itype(lineSplit,PC,label_addr),2)
+        elif lineSplit[1] == 'halt' or lineSplit[1] == 'noop' :
+            mem[PC] = int(c.Otype(lineSplit),2)
+        elif lineSplit[1] == 'jalr':
+            mem[PC] = int(c.Jtype(lineSplit),2)
+        elif lineSplit[1] == '.fill':
+            
+            if lineSplit[2].lstrip('-').isdigit():                             #ถ้าตัวหลังจาก .fill เป็นเลข เอามาใส่เลย
+                mem[PC] = int(lineSplit[2])                                            
+            else:                                     
+                mem[PC] = int(label_addr[lineSplit[2]])                        #ถ้าเป็น (symbolic address)string เอาไปเช็คกับ (int)label_addr ที่ map ไว้
+        else:
+            print("Invalid instruction >> label " + str(PC+1) )
+            exit(1)
+
